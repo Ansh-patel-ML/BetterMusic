f6257699db330f293b9b373a91934da19a12b20c
Ansh-patel-ML <patelansh883@gmail.com>
2023-11-30 08:49:24 +0530

    Initial setup for the home screen

---
+++ components/Header.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            className="rounded-full bg-black flex justify-center items-center hover:opacity-75 transition"
+            onClick={() => router.back()}
+          >
+            <RxCaretLeft className="text-white" size={35} />
+          </button>
+          <button
+            className="rounded-full bg-black flex justify-center items-center hover:opacity-75 transition"
+            onClick={() => router.forward()}
+          >
+            <RxCaretRight className="text-white" size={35} />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="bg-white flex justify-center items-center p-2 hover:opacity-75 transition rounded-full">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="bg-white flex justify-center items-center p-2 hover:opacity-75 transition rounded-full">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
